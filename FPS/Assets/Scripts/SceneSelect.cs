0d524c2b4527480cee966a58f860864c34125980
Shu-Nogami <g2053930@cc.kyoto-su.ac.jp>
2023-04-22 17:35:41 +0900

    [FIX]シーン名変更とシーン遷移をシーン名で行うように変更

--- FPS/Assets/Scripts/SceneSelect.cs
+++ FPS/Assets/Scripts/SceneSelect.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneSelect : MonoBehaviour
@@ -8,14 +6,8 @@ public class SceneSelect : MonoBehaviour
     void OnTriggerEnter(Collider other){
         //プレイヤーが指定のオブジェクトに乗った時
         if(other.name == "FPSController"){
-            //シーン CPUに移動
-            if(transform.name == "TDM"){
-                SceneManager.LoadScene("CPU");
-            }
-            //シーン Training rommに移動
-            else if(transform.name == "Training room"){
-                SceneManager.LoadScene("Training room");
-            }
+            //シーン移動
+            SceneManager.LoadScene(transform.name);
         }
     }
 }
